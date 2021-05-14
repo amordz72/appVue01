ff45b4abf15da9213d4a43e45dbd59e24cfd2bf4
amordz72 <50753427+amordz72@users.noreply.github.com>
2021-05-14 23:12:42 +0100

    01

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ msg }}</h1>
+ <h1  >{{title}}</h1>
+  <p>
+ <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+  <h3>{{ state.count }}</h3>
+ <button @click="state.count++"> ++</button>
+  <button @click="state.count--"> - - </button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String,    title: String,  
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+h3{
+ 
+}
+
+
+</style>
